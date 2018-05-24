453887a8c43d5eabb405d328e140c80f6ab2187a
claudioDcv <claudio.dcv@gmail.com>
2018-05-24 01:31:51 -0400

    added period all views

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -4,9 +4,9 @@ from apps.core.views import ProgramIndexView, CourseView, EvaluatedIndexView, Ev
 urlpatterns = [
     path('program/<slug:code>/<profile>/<int:period>', ProgramIndexView.as_view(), name='program-index'),
 
-    path('program/<slug:code>/<profile>', ProgramIndexView.as_view(), name='program-index'),
-    path('program/<slug:code>/<profile>/course/<pk>', CourseView.as_view(), name='course-index'),
+    path('program/<slug:code>/<profile>/period/<int:period>', ProgramIndexView.as_view(), name='program-index'),
+    path('program/<slug:code>/<profile>/course/<pk>/period/<int:period>', CourseView.as_view(), name='course-index'),
     path('program/<slug:code>/<profile>/skill-list', SkillGroupIndexView.as_view(), name='skill-index'),
-    path('program/<slug:code>/<profile>/course/<course_id>/evaluated/<pk>', EvaluatedIndexEvaluatorView.as_view(), name='evaluated-evaluator-index'),
-    path('program/<slug:code>/<profile>/course/<course_id>/detail', EvaluatedIndexView.as_view(), name='evaluated-index'),
+    path('program/<slug:code>/<profile>/course/<course_id>/evaluated/<pk>/period/<int:period>', EvaluatedIndexEvaluatorView.as_view(), name='evaluated-evaluator-index'),
+    path('program/<slug:code>/<profile>/course/<course_id>/detail/period/<int:period>', EvaluatedIndexView.as_view(), name='evaluated-index'),
 ]
